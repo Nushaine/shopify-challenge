4f983b8a20d85c0c168b941b16a6084e885ceb49
Nushaine Ferdinand <nushaine@gmail.com>
2022-01-13 00:16:47 -0500

    style improvements & small fixes

--- components/tooltip.tsx
+++ components/tooltip.tsx
@@ -1,11 +1,11 @@
 import React from "react"
-  
+
 export default function ToolTip(props: {text: string}) {
   return (
     <div className="tooltip-container flex-col-hcenter">
       <img
-        src="https://s3-us-west-2.amazonaws.com/figma-alpha-api/img/d404/dfc9/b236514c01cc6f1df2b92061dafaa9e9"
-        alt="image not found"
+        src="/arrow.svg"
+        alt="arrow"
         className="arrow"
       />
       <div className="tooltop-text flex-row-vcenter-hcenter">
